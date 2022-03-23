96ba9316b872151a25eac153b18e9de62a5ae92d
Adi Yenuubarii <74401392+adiyenuubarii@users.noreply.github.com>
2022-03-23 16:03:05 +0530

    Create script.js

---
+++ HTML-JS rick roll/script.js
@@ -0,0 +1,8 @@
+// RickRoll!!
+const url = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+
+let linkClicker = document.getElementById('link_click');
+
+window.onload = window.onfocus = () => {
+  this.document.location.href = url;
+}
